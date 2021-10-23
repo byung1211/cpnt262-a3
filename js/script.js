d6487b0b640c029b1b940363d49c1d7be9de849a
byung1211 <byung1211@edu.sait.ca>
2021-10-23 12:11:52 -0600

    Temporary commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+// const picsumIds = [
+// {
+//   id:237,
+//   title:'Puppy',
+//   src:'237.jpg'
+// },
+// {
+//   id:237,
+//   title:'Puppy',
+//   src:'237.jpg'
+// }
+
+// ];
+
+// let output = '';
+// picsumIds.forEach(function(animal){
+
+//   console.log(animal.title);
+
+//   output += `
+//   <a href="[off-site link]">
+//     <img src="https://picsum.photos/id/${animal.id}/300/300" alt="${animal.title} Photo">
+//   </a>  
+//   `;
+
+// });
+
+// const gallery = document.querySelector('.gallery');
+// gallery.innerHTML = output;
